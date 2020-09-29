9d0d887d3feecc35b3113d827b6206a9002debad
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-09-29 10:05:09 +0100

    Test3

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -160,7 +160,7 @@ class Main extends Component {
             padding: "30px",
           }}
         >
-          Candle-data 2020 Test2
+          Candle-data 2020 Test3
         </footer>
       </div>
     );

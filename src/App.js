52558f217364751394cc09539a53dfed0163a285
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-09-29 20:15:25 +0100

    Updated routing

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
 import Main from "./components/main";
 import { Navigation } from "./common";
 import CandleDataTimeline from "./components/candleDataTimeline";
 
+const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <Router>
-      <Navigation basename={process.env.PUBLIC_URL}/>
+    <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+      <Navigation basename={process.env.PUBLIC_URL} />
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/timeline" component={CandleDataTimeline} />
         <Route component={() => <div>404 Not found</div>} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   </ThemeProvider>
 );
 

081581999d62599b7fac886b73ac29f9e1deeb53
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-09-29 11:28:45 +0100

    Updates

---
+++ precache-manifest.1fcade5defc5d28c77132761664ae395.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8fded7abed74eaac457a3284e94ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d97103099adc7c615ba9",
+    "url": "/static/css/2.42a5fdae.chunk.css"
+  },
+  {
+    "revision": "33ca27e4a9051d96953e",
+    "url": "/static/css/main.fadbe864.chunk.css"
+  },
+  {
+    "revision": "d97103099adc7c615ba9",
+    "url": "/static/js/2.f7d64cbd.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.f7d64cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ca27e4a9051d96953e",
+    "url": "/static/js/main.32db7356.chunk.js"
+  },
+  {
+    "revision": "a058d9c1e4990d27ace2",
+    "url": "/static/js/runtime-main.ce04e98c.js"
+  },
+  {
+    "revision": "06d304566f54f8b30dcc270f108c934a",
+    "url": "/static/media/ChunkFivePrint.ttf.06d30456.svg"
+  },
+  {
+    "revision": "f385330919c8fdb9ef9a3a55a792e3d1",
+    "url": "/static/media/ChunkFivePrint.ttf.f3853309.eot"
+  },
+  {
+    "revision": "f7f089dfc840ad2f5a284e9dfb4857a8",
+    "url": "/static/media/ChunkFivePrint.ttf.f7f089df.woff"
+  },
+  {
+    "revision": "03a952bc813ff9be711c2b9f9b59edf8",
+    "url": "/static/media/candlestick1.03a952bc.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

43d8c3e74e195a269e4495989277cc62bcbf46bd
swdpcomputing <67058346+swdpcomputing@users.noreply.github.com>
2020-10-08 19:13:07 +0100

    Added test.each style to paginate.js

--- src/utils/paginate.test.js
+++ src/utils/paginate.test.js
@@ -1,7 +1,18 @@
 const { paginate } = require("./paginate.js");
 
+test("Type checks", () => {
+  expect;
+});
+
 test("Correct Pagination returns", () => {
-  //prettier-ignore
-  expect(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 6)).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 4)).toEqual([1, 2, 3, 4]);
   expect(paginate([1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 3)).toEqual([4, 5, 6]);
 });
+
+//prettier-ignore
+//tests the same as above, but not readable
+test.each([[[1, 2, 3, 4, 5, 6, 7, 8, 9], 1, 4, [1, 2, 3, 4]], [[1, 2, 3, 4, 5, 6, 7, 8, 9], 2, 3, [4, 5, 6]]])(
+    '%i pagainated with %i and %i equals %i', (a, b, c, d) => {
+        expect(paginate(a, b, c)).toEqual(d)
+    },
+  );
